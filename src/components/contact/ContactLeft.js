dc13a91dc914575227cf8f8138828f7311f7fbdf
Gadisa <gadaboma@gmail.com>
2024-04-27 20:10:21 +0300

    minor changes

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
-import { contactImg } from "../../assets/index";
+import { bannerImg } from "../../assets/index";
 
 const ContactLeft = () => {
   return (
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
       <img
-        className="w-full h-64 object-cover rounded-lg mb-2"
-        src={contactImg}
+        className="w-full h-84 object-cover rounded-lg mb-2"
+        src={bannerImg}
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
@@ -15,12 +15,7 @@ const ContactLeft = () => {
         <p className="text-lg font-normal text-gray-400">
           Software Engineer - Full Stack Engineer
         </p>
-        <p className="text-base text-gray-400 tracking-wide">
-          I'm Passionate and innovative Full Stack Software Engineer with a
-          strong proficiency in mobile and web application development. Expert
-          in Flutter, Reactjs, Nodejs, Nestjs, I specialize in crafting dynamic,
-          user-centric solutions that enhance engagement and functionality..
-        </p>
+
         <p className="text-base text-gray-400 flex items-center gap-2">
           Phone: <span className="text-lightText">+251967436185</span>
         </p>
@@ -31,7 +26,7 @@ const ContactLeft = () => {
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-        <span className="bannerIcon">
+          <span className="bannerIcon">
             <a
               target="_BLANK"
               href="https://www.linkedin.com/in/gadisa-aboma-5b7934192/"
